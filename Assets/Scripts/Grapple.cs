cb1bbfcf84565270774abc72e131f5d3dc899f58
SRBoyer <srb96@hotmail.com>
2021-10-31 16:38:09 -0400

    Implement raycast collision detection to grapple

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -9,6 +9,11 @@ public class Grapple : MonoBehaviour
     public LineRenderer _lineRenderer;
     public SpringJoint2D _springJoint;
 
+    // The max range of the grapple
+    public float maxDistance = 1.0f;
+
+    [SerializeField] private LayerMask grappleLayerMask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +24,23 @@ public class Grapple : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             // convert mouse position to world position
-            Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 targetPos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
-            // set origin and target points of line renderer
-            _lineRenderer.SetPosition(0, mousePos);
-            _lineRenderer.SetPosition(1, transform.position);
+            if(DetectGrapplePoint(transform.position, ref targetPos))
+            {
+                // set origin and target points of line renderer
+                _lineRenderer.SetPosition(0, targetPos);
+                _lineRenderer.SetPosition(1, transform.position);
 
-            _springJoint.connectedAnchor = mousePos;
-            _springJoint.enabled = true;
-            _lineRenderer.enabled = true;
+                _springJoint.connectedAnchor = targetPos;
+                _springJoint.enabled = true;
+                _lineRenderer.enabled = true;
+            }
+            
         }
         else if (Input.GetKeyUp(KeyCode.Mouse0))
         {
@@ -38,9 +48,29 @@ public class Grapple : MonoBehaviour
             _lineRenderer.enabled = false;
         }
 
-        if (_springJoint.enabled)
+        if (_springJoint.enabled) // update position of origin, i.e.: when the button is held down
         {
             _lineRenderer.SetPosition(1, transform.position);
         }
     }
+
+    // Casts a ray going from origin towards target and changes target to the location of the first
+    // collision detected. The raycast goes beyond the target, until/unless maxDistance is reached
+    private bool DetectGrapplePoint(Vector2 origin, ref Vector2 target)
+    {
+        Vector2 asDirection = target - origin;
+        RaycastHit2D hit = Physics2D.Raycast(origin, asDirection, maxDistance, grappleLayerMask);
+        
+        if (hit.collider != null)
+        {
+            // change grapple point
+            target = hit.point;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
