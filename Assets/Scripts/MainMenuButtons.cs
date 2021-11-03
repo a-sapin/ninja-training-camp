548b59963103ef8b88841d3746ceafa67dcff13e
SRBoyer <srb96@hotmail.com>
2021-11-02 21:47:06 -0400

    Add main menu buttons script

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    public static bool wantsToQuit = false;
+
+    public void PlayGame()
+    {
+
+    }
+
+    public void AccessOptionsMenu()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        wantsToQuit = true;
+        StartCoroutine(DelayedQuit());
+    }
+
+    private IEnumerator DelayedQuit()
+    {
+        yield return null;
+        Application.Quit();
+    }
+}
