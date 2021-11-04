534208af2108105a79f5f8d5fe1f02ca7bfb235a
SRBoyer <srb96@hotmail.com>
2021-11-04 19:24:42 -0400

    Set hasGrapplePower to true in Start function

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -55,6 +55,7 @@ public class PlayerLocomotion : MonoBehaviour
         holdingJump = false;
         hasDashPower = true;
         hasDoubleJumpPower = true;
+        hasGrapplePower = true;
 
         dashCooldownTimer = 0.0f;
         isDashing = false;
