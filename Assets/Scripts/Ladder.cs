3c4280cb69ba7ec1c4ddd5c26b2e5463942d6f41
SRBoyer <srb96@hotmail.com>
2021-11-30 14:54:04 -0500

    Add layered collider to prefab

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ladder : MonoBehaviour
 {
     private LineRenderer lr;
+    private BoxCollider2D myBox;
     [SerializeField] Transform top;
     [SerializeField] Transform bottom;
 
@@ -12,6 +13,7 @@ public class Ladder : MonoBehaviour
     void Start()
     {
         lr = GetComponent<LineRenderer>();
+        myBox = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -19,5 +21,11 @@ public class Ladder : MonoBehaviour
     {
         lr.SetPosition(0, top.localPosition);
         lr.SetPosition(1, bottom.localPosition);
+
+        float size_y = top.localPosition.y - bottom.localPosition.y;
+        float offset_y = size_y / 2.0f;
+
+        myBox.size = new Vector2(1, size_y);
+        myBox.offset = new Vector2(0, offset_y);
     }
 }
