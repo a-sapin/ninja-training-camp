bdf4522930159fd223421bdf4c62f8c69d5f2eb1
Samuel Bertrand <soccer-samuel@hotmail.com>
2022-01-26 19:17:37 -0500

    Change Level Name + physics of falling object

--- Assets/FallingRock.cs
+++ Assets/FallingRock.cs
@@ -8,15 +8,16 @@ public class FallingRock : MonoBehaviour
     [SerializeField] private int blastZoneYLevel = -50;
     [SerializeField] [Range(0.01f, 5)] private float knockbackDuration = 1.0f;
     [SerializeField] [Range(0.01f, 100)] private float knockbackForce = 1.0f;
-    private void OnTriggerEnter2D(Collider2D col)
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.CompareTag("Player"))
+        if (col.collider.CompareTag("Player"))
         {
             Vector2 pushingDirection = (gameObject.transform.position - col.transform.position).normalized;
-            StartCoroutine(col.GetComponent<PlayerLocomotion>().PlayerKnockback(knockbackDuration,knockbackForce,pushingDirection));
+            StartCoroutine(col.collider.GetComponent<PlayerLocomotion>().PlayerKnockback(knockbackDuration,knockbackForce,pushingDirection));
         }
     }
 
+  
     private void Update()
     {
         if (gameObject.transform.position.y < blastZoneYLevel)
