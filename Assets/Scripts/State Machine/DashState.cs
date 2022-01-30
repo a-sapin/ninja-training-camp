0e46a753f97c4146080f769e3ea2b248fed4e344
SRBoyer <srb96@hotmail.com>
2022-01-29 22:08:35 -0500

    Implement dash state

--- Assets/Scripts/State Machine/DashState.cs
+++ Assets/Scripts/State Machine/DashState.cs
@@ -7,9 +7,33 @@ using UnityEngine;
 /// </summary>
 public class DashState : AirDriftState
 {
+    Vector2 dashDirection = Vector2.zero;
+    float horizontalVelocity;
+
+    public override void Enter(PlayerManager player)
+    {
+        base.Enter(player);
+        dashDirection = new Vector2(player.GetInput().Move().x, 0f); // save horizontal player input
+        dashDirection.Normalize();
+        horizontalVelocity = player.GetLocomotion().GetVelocity().x; // save player horizontal velocity as they start dashing
+        if(horizontalVelocity > player.GetLocomotion().maxVelocity)
+        {
+            horizontalVelocity = player.GetLocomotion().maxVelocity; // clamp velocity to max
+        }
+        player.GetLocomotion().StartDash(dashDirection);
+    }
+
+    public override void Exit(PlayerManager player)
+    {
+        player.GetLocomotion().EndDash();
+    }
+
     public override void HandleSurroundings(PlayerManager player)
     {
-
+        if(Time.time - startTime >= player.DashDuration)
+        {
+            base.HandleSurroundings(player); // return to airdrift or running state when dash is finished
+        }
     }
 
     public override void HandleInputs(PlayerManager player)
