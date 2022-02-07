a2789aef48087740e29743a0b9521dc57e9a94b1
SRBoyer <srb96@hotmail.com>
2022-02-06 20:32:07 -0500

    Implement ladder climb state

--- Assets/Scripts/State Machine/LadderClimbState.cs
+++ Assets/Scripts/State Machine/LadderClimbState.cs
@@ -2,35 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LadderClimbState : State
+public class LadderClimbState : LadderGrabState
 {
-    private bool isLerping = true;
-    public override void Enter(PlayerManager player)
-    {
-        base.Enter(player);
-        // start of climbing animation
-        // start Lerp to ladder
-        isLerping = true;
-    }
-
     public override void HandleSurroundings(PlayerManager player)
     {
-        //if is pressing left or right and not lerping, go to airdrift
-    }
-
-    public override void HandleInputs(PlayerManager player)
-    {
-        // if holding up or down, climb ladder
+        if (!player.GetLocomotion().IsTouchingLadder()) // not touching ladder
+        {
+            player.ChangeState(MovementState(player));
+        }
     }
 
     public override void LogicUpdate(PlayerManager player)
     {
-        // if is lerping, lerp
-        // otherwise move up, down or not at all
+        player.GetLocomotion().ClimbLadder(player.GetInput().Move(), Time.deltaTime);
     }
 
-    public override void PhysicsUpdate(PlayerManager player)
-    {
-        // do we need this function?
-    }
 }
