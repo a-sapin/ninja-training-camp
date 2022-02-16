ddcb494958111554ae7346a6b580dab17053e226
Arthur 'RainbowSuperLoki' Sapin <rainbowsuperloki@gmail.com>
2022-02-15 21:35:55 -0500

    First version of new dialogue (lowkey spaghetti)

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -3,18 +3,20 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-[Serializable] class DialoguePart
+[Serializable]
+class DialoguePart
 {
     public bool isSenseiTalking;
     public string text;
 }
 public class Dialogue : MonoBehaviour
 {
-    [SerializeField] private int currentLevel = 1;
     [SerializeField] private Text textZone;
     [SerializeField] private GameObject sensei, player, canvas;
     [SerializeField] private DialoguePart[] dialogue;
+    [SerializeField] private float writeDelay; //LOKI
     private PlayerManager playerManager;
+    VFXManager mySoundManager;
 
     void Start()
     {
@@ -23,11 +25,6 @@ public class Dialogue : MonoBehaviour
 
     private void StartFirstDialogue()
     {
-        if (PlayerPrefs.GetInt("playDialogue" + currentLevel, 1) != 1)
-        {
-            canvas.SetActive(false);
-            return;
-        }
         IEnumerator firstDialogue = PlayDialogue(dialogue);
         StartCoroutine(firstDialogue);
     }
@@ -35,30 +32,53 @@ public class Dialogue : MonoBehaviour
     IEnumerator PlayDialogue(DialoguePart[] dialogue)
     {
         yield return new WaitForSecondsRealtime(0.05f);
+        mySoundManager = FindObjectOfType<VFXManager>(); //Find sound manager for dialogue "beep"
         playerManager = FindObjectOfType<PlayerManager>();
         playerManager.LockGameplayInput(); // freeze player during dialogue
         //lock
         int dialogueIndex = 0;
         canvas.SetActive(true);
-        if(dialogue.Length > 0)
+        if (dialogue.Length > 0)
         {
-            textZone.text = dialogue[dialogueIndex].text;
+
             sensei.SetActive(dialogue[dialogueIndex].isSenseiTalking);
             player.SetActive(!dialogue[dialogueIndex].isSenseiTalking);
+
+            //LOKI (remove when finished)
+            int lengthOfSentence = dialogue[dialogueIndex].text.Length;
+            char[] charsArray = new char[lengthOfSentence];
+            for (int i = 0; i < lengthOfSentence; i++) //Put each char of DIALOGUE TEXT into a slot of the array
+            {
+                charsArray[i] = dialogue[dialogueIndex].text[i];
+            }
+
+            string current_Text = "";
+
+            for (int o = 0; o < lengthOfSentence; o++) //Append each char of the dialogue to current_text then display it until everything is here
+            {
+                current_Text = current_Text + charsArray[o]; //Append char
+                textZone.text = current_Text;
+                mySoundManager.Play("Talking");
+                yield return new WaitForSecondsRealtime(writeDelay);
+            }
+
+            //LOKI (remove when finished)
+
+            //textZone.text = dialogue[dialogueIndex].text;
         }
         yield return new WaitForSecondsRealtime(0.5f);
         while (true)
         {
             if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Escape))
             {
-               
+
                 dialogueIndex++;
                 if (dialogueIndex >= dialogue.Length) break;
 
                 textZone.text = dialogue[dialogueIndex].text;
                 sensei.SetActive(dialogue[dialogueIndex].isSenseiTalking);
                 player.SetActive(!dialogue[dialogueIndex].isSenseiTalking);
-                
+
                 yield return new WaitForSecondsRealtime(0.5f);
             }
             yield return null;
