faf4303cc2e0dd20b22ccf4c6e82f7c5e7e9cbbb
Samuel Bertrand <soccer-samuel@hotmail.com>
2022-03-09 12:13:34 -0500

    Updated pendulum to feel more natural

--- Assets/PendulumSwing.cs
+++ Assets/PendulumSwing.cs
@@ -4,42 +4,16 @@ using UnityEngine;
 
 public class PendulumSwing : MonoBehaviour
 {
-    //[SerializeField] private Rigidbody2D rigidbody2D;
-    [SerializeField] [Range(-1,-0f)] [Tooltip("Angle Gauche maximal qui le swing peu aller")] float leftSwingRange;
-    [SerializeField] [Range(0f,1)] [Tooltip("Angle Droit maximal qui le swing peu aller")]private float rightSwingRange;
-    [SerializeField] [Range(0,1)] private float velocityMax;
+    [SerializeField] private Rigidbody2D rigidbody2D;
+    private float velocityMax;
+    [SerializeField] private float speed;
     
-    // Update is called once per frame
     void Update()
     {
-        //Swing();
-        if (transform.rotation.normalized.z > rightSwingRange && velocityMax > 0) 
-            velocityMax *= -1;
-        if (transform.rotation.normalized.z < leftSwingRange && velocityMax < 0) 
-            velocityMax *= -1;
-        
+        velocityMax = speed * Mathf.Sin(Time.unscaledTime);
+        rigidbody2D.angularVelocity = velocityMax;
         transform.Rotate(Vector3.forward, velocityMax);
-        
+
     }
 
-    /*  DEPRECATED FUNCTION. Je vais still laisser la si je veux attemp une swing plus "naturelle"
-    private void Swing()
-    {
-        if (transform.rotation.z > 0 &&
-            transform.rotation.eulerAngles.z < rightSwingRange &&
-            rigidbody2D.angularVelocity > 0 &&
-            rigidbody2D.angularVelocity < velocityMax)
-        {
-            rigidbody2D.angularVelocity = velocityMax;
-        }
-        else if (transform.rotation.z < 0 &&
-                 transform.rotation.eulerAngles.z > leftSwingRange &&
-                 rigidbody2D.angularVelocity < 0 &&
-                 rigidbody2D.angularVelocity > velocityMax * -1)
-        {
-            rigidbody2D.angularVelocity = -velocityMax;
-        }
-
-
-    }*/
 }
