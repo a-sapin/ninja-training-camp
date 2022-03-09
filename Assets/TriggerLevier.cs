8fdbe15737069cefccbe406855dbca7f64f18b48
Samuel Bertrand <soccer-samuel@hotmail.com>
2022-03-09 14:54:41 -0500

    Made generic level system that lerps in a direction on x time

--- Assets/TriggerLevier.cs
+++ Assets/TriggerLevier.cs
@@ -1,26 +1,66 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerLevier : MonoBehaviour
 {
-    [SerializeField] private GameObject associatedGO;
+    [SerializeField] private GameObject associatedGo;
     [SerializeField] private TypeLevier typeLevier;
 
-    enum TypeLevier
+    [SerializeField] private float timer = 1;
+    [SerializeField] private float distanceToMove = 1;
+    [SerializeField] private bool doOnce = false;
+
+    private bool isActivated;
+    private enum TypeLevier
     {
-        RaiseWall
-    };
-   private void OnCollisionEnter2D(Collision2D col)
-       {
-           if (col.gameObject.CompareTag("Player"))
-           {
-               switch (typeLevier)
-               {
-                   case TypeLevier.RaiseWall:
-                      associatedGO.transform.Translate(associatedGO.transform.up);
-                      break;
-               }
-           }
-       }
+        RaiseWall,
+        MoveRight,
+        MoveLeft
+    }
+
+    private void Start()
+    {
+        isActivated = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            
+
+            if (isActivated) return;
+            switch (typeLevier)
+            {
+                case TypeLevier.RaiseWall:
+                    Debug.Log("TiggerLevier || RaiseWall");
+                    StartCoroutine(RaiseWall(timer));
+                    break;
+                case TypeLevier.MoveRight:
+                    break;
+                case TypeLevier.MoveLeft:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            if(doOnce)
+                isActivated = true;
+        }
+    }
+
+    private IEnumerator RaiseWall(float delayTime)
+    {
+        var startTime = Time.time;
+        while (Time.time - startTime <= delayTime)
+        {
+            var position = associatedGo.transform.position;
+            position = Vector3.Lerp(position,
+                position + associatedGo.transform.up * Time.deltaTime * distanceToMove,
+                Time.time - startTime);
+            associatedGo.transform.position = position;
+            yield return null;
+        }
+    }
 }
