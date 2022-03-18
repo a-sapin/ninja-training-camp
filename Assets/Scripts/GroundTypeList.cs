adb548657e8ca057011ff46fb142bb654a22dc79
SRBoyer <srb96@hotmail.com>
2022-03-18 19:26:51 -0400

    Create first GroundTypeList object

---
+++ Assets/Scripts/GroundTypeList.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "New Ground Type")]
+public class GroundTypeList : ScriptableObject
+{
+    [SerializeField] TileBase[] tiles;
+
+    /// <summary>
+    /// Checks if a tile is in this list.
+    /// </summary>
+    /// <param name="theTile">The tile we want to know is part of this list</param>
+    /// <returns>True or False</returns>
+    public bool Contains(TileBase theTile)
+    {
+        foreach (TileBase currentTileInArray in tiles)
+        {
+            if (theTile.Equals(currentTileInArray))
+                return true;
+        }
+        return false;
+    }
+}
