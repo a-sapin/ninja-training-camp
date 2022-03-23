e8217ca29a3f2707c2a0e196cb33d450433592eb
SRBoyer <srb96@hotmail.com>
2022-03-23 15:46:50 -0400

    Add ground type enum and generic function

--- Assets/Scripts/FootstepHandler.cs
+++ Assets/Scripts/FootstepHandler.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+public enum GroundType
+{
+    GRASS,
+    STONE,
+    WOOD,
+    DIRT,
+    NULL // is NULL necessary?
+}
+
 public class FootstepHandler : MonoBehaviour
 {
     [SerializeField] GroundTypeList grassTiles;
@@ -10,21 +19,60 @@ public class FootstepHandler : MonoBehaviour
     [SerializeField] GroundTypeList woodTiles;
     [SerializeField] GroundTypeList dirtTiles;
 
+    /// <summary>
+    /// EXPENSIVE, USE WITH CAUTION
+    /// </summary>
+    public GroundType DetermineGroundType(TileBase currentGround)
+    {
+        if (IsGrass(currentGround))
+        {
+            return GroundType.GRASS;
+        }
+        else if (IsStone(currentGround))
+        {
+            return GroundType.STONE;
+        }
+        else if (IsWood(currentGround))
+        {
+            return GroundType.WOOD;
+        }
+        else if (IsDirt(currentGround))
+        {
+            return GroundType.DIRT;
+        }
+        else
+        {
+            return GroundType.NULL;
+        }
+    }
+
+    /// <summary>
+    /// EXPENSIVE, USE WITH CAUTION
+    /// </summary>
     public bool IsGrass(TileBase currentGround)
     {
         return grassTiles.Contains(currentGround);
     }
 
+    /// <summary>
+    /// EXPENSIVE, USE WITH CAUTION
+    /// </summary>
     public bool IsStone(TileBase currentGround)
     {
         return stoneTiles.Contains(currentGround);
     }
 
+    /// <summary>
+    /// EXPENSIVE, USE WITH CAUTION
+    /// </summary>
     public bool IsWood(TileBase currentGround)
     {
         return woodTiles.Contains(currentGround);
     }
 
+    /// <summary>
+    /// EXPENSIVE, USE WITH CAUTION
+    /// </summary>
     public bool IsDirt(TileBase currentGround)
     {
         return dirtTiles.Contains(currentGround);
