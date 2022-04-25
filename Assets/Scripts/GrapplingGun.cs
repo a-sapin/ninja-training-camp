dc52115d60bdfdd21afdade2347e353594f277f5
SRBoyer <srb96@hotmail.com>
2022-04-24 21:06:17 -0400

    Move controller grapple function
    Also fix grapple power loss not working

--- Assets/Scripts/GrapplingGun.cs
+++ Assets/Scripts/GrapplingGun.cs
@@ -61,39 +61,56 @@ public class GrapplingGun : MonoBehaviour
 
     private void Update()
     {
-        if (playerRef.IsActionable() && playerRef.getState()!=State.ladderClimb && playerRef.getState()!=State.ladderGrab)
-        {
-            //event appeler lorsque le joueur utilise clic gauche pour la grapple
-            if (Input.GetKeyDown(KeyCode.Mouse0) && playerRef.CanGrapple())
-            {
-                SetGrapplePoint(Input.mousePosition);
-            }
-            else if ((Input.GetKey(KeyCode.Mouse0) || isGrapplingWithPad) &&
-                     playerRef.CanGrapple()) //appeler lorsque le joueur maintien le clic gauche
-            {
-                Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
-                RotateGun(mousePos, true); //utiliser pour avoir le vecteur joueur-clic
+        ReadInputAndActivateGrapple(Time.deltaTime);
+    }
 
-                if (launchToPoint && grappleRope.isGrappling)
+    private void ReadInputAndActivateGrapple(float delta)
+    {
+        //event appeler lorsque le joueur utilise clic gauche pour la grapple
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !isGrapplingWithPad && playerRef.CanGrapple())
+        {
+            SetGrapplePoint(Input.mousePosition);
+        }
+        else if (Input.GetKey(KeyCode.Mouse0) && !isGrapplingWithPad 
+            && playerRef.CanGrapple()) //appeler lorsque le joueur maintien le clic gauche
+        {
+            Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+            RotateGun(mousePos, true); //utiliser pour avoir le vecteur joueur-clic
+
+            if (launchToPoint && grappleRope.isGrappling)
+            {
+                if (launchType == LaunchType.Transform_Launch)
                 {
-                    if (launchType == LaunchType.Transform_Launch)
-                    {
-                        Vector2 firePointDistnace = firePoint.position - gunHolder.localPosition;
-                        Vector2 targetPos = grapplePoint - firePointDistnace;
-                        gunHolder.position = Vector2.Lerp(gunHolder.position, targetPos, Time.deltaTime * launchSpeed);
-                    }
+                    Vector2 firePointDistnace = firePoint.position - gunHolder.localPosition;
+                    Vector2 targetPos = grapplePoint - firePointDistnace;
+                    gunHolder.position = Vector2.Lerp(gunHolder.position, targetPos, delta * launchSpeed);
                 }
             }
-            else if (Input.GetKeyUp(KeyCode.Mouse0) &&
-                     playerRef.CanGrapple()) //appeler lorsque le joueur relache le clic gauche
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0) && !isGrapplingWithPad
+           && playerRef.CanGrapple()) //appeler lorsque le joueur relache le clic gauche
+        {
+            StopGrappling();
+        }
+        else if (Input.GetAxis("Grapple") > 0 && !isGrapplingWithPad && playerRef.CanGrapple())
+        {
+            Vector2 nearestTarget = GetNearestTargetPos(transform.position);
+
+            if (Vector2.Distance(nearestTarget, transform.position) < maxDistnace)
             {
-                StopGrappling();
+                //LANCER LE GRAPPIN
+                Debug.Log("GrapplingGun.cs || Grappling");
+                SetGrapplePoint(nearestTarget, true);
             }
-            else
-            {
-                Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
-                RotateGun(mousePos, true);
-            }
+        }
+        else if (Input.GetAxis("Grapple") <= 0 && isGrapplingWithPad)
+        {
+            StopGrappling();
+        }
+        else
+        {
+            Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+            RotateGun(mousePos, true);
         }
     }
 
